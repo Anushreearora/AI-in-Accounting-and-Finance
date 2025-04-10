e6a633f8c76cb5581f9b10577640689b50becc82
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2025-04-10 14:57:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request,render_template
+import textblob
 
 app = Flask(__name__)
 
@@ -7,6 +8,24 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    return(render_template("prediction.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r=(-50.6*q)+90.2))
+
+@app.route("/sentiment_analysis",methods=["GET","POST"])
+def sentiment_analysis():
+    return(render_template("sentiment_analysis.html"))
+
+@app.route("/sentiment_result",methods=["GET","POST"])
+def sentiment_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("sentiment_result.html", r=r))
+
 if __name__ == "__main__":
     app.run()
-    
